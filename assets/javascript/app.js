7339cf8db16f5f20fe63fb8b80f3a8949c42dff6
Cadin McQueen <cadin.mcqueen@gmail.com>
2018-09-19 19:38:17 -0400

    Add functional firebase storage of user data (#5)
    * add inputs on index.html
    
    * Add bootstrap positioning and some temporary quality of life styling
    
    * Added funtioning google maps
    
    * finished user input map
    
    * Add logic that pulls values from form and input map and sends them to Firebase as an object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,114 @@
 // Google maps API key: AIzaSyC6GnZhTZ8RV_-JINWYAo0e2KDJQQoz7h0
 
 var config = {
-  apiKey: "AIzaSyBJUt98o2qHuICn_Ab3av5kXrG0N1lrL_4",
-  authDomain: "pet-finder-6e201.firebaseapp.com",
-  databaseURL: "https://pet-finder-6e201.firebaseio.com",
-  projectId: "pet-finder-6e201",
+  apiKey: "AIzaSyDDqeSFBecW0z7ho68WE5ZwcTusl6hVits",
+  authDomain: "musician-finder-f0e28.firebaseapp.com",
+  databaseURL: "https://musician-finder-f0e28.firebaseio.com",
+  projectId: "musician-finder-f0e28",
   storageBucket: "",
-  messagingSenderId: "434407968868"
+  messagingSenderId: "495490109784"
 };
 
 firebase.initializeApp(config);
 
 database = firebase.database();
+
+var latInput = 0;
+var longInput = 0;
+
+function initMap() {
+
+  var philly = { lat: 39.953, lng: -75.165 };
+
+  var map = new google.maps.Map(
+    document.getElementById('main-map'), { zoom: 13, center: philly });
+  var map2 = new google.maps.Map(
+    document.getElementById('map-input'), { zoom: 14, center: philly });
+
+  var contentString = "<p>Test</p>"
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+    position: philly,
+    map: map,
+  });
+  var marker2 = new google.maps.Marker({
+    position: { lat: 39.983, lng: -75.165 },
+    map: map,
+  });
+  marker2.addListener('click', function () {
+    infowindow.open(map2, marker2);
+  });
+
+  var placemarker;
+  var markerPlaced = false;
+
+  function placeMarker(location) {
+    if (placemarker) {
+      placemarker.setPosition(location);
+    } else {
+      placemarker = new google.maps.Marker({
+        position: location,
+        map: map2
+      });
+     }
+
+    latInput = placemarker.getPosition().lat();
+    longInput = placemarker.getPosition().lng();
+    console.log("Latitude: " + latInput + " | Longitute: " + longInput)
+  }
+
+
+  google.maps.event.addListener(map2, 'click', function (event) {
+    placeMarker(event.latLng);
+  });
+}
+
+$(document).ready(function() {
+  $("#submit-btn").on("click", function(event) {
+    event.preventDefault();
+
+    var name = $('#name-input').val().trim();
+    var email = $('#email-input').val().trim();
+    var instrument = $('#instrument-input').val().trim();
+    var experience = $('#experience-input').val().trim();
+    var youtube = $('#youtube-input').val().trim();
+    var description = $('#description-input').val().trim();
+
+    var newMusician = {
+      name: name,
+      email: email,
+      instrument: instrument,
+      experience: experience,
+      youtube: youtube,
+      description: description,
+      position: {
+        lat: latInput,
+        long: longInput
+      }
+    }
+
+    database.ref().push(newMusician);
+
+    $('#name-input').val("")
+    $('#email-input').val("")
+    $('#instrument-input').val("")
+    $('#experience-input').val("")
+    $('#youtube-input').val("")
+    $('#description-input').val("")
+  });
+
+  database.ref().on("child_added", function(snapshot) {
+    // Create main-map markers here
+
+
+    
+  });
+
+    
+
+
+});
