11dc30273888f7e39887d4f540767bf6204dd77c
Jason Tashman <jtashman703@gmail.com>
2018-09-19 18:15:05 -0400

    Added funtioning google maps

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,3 +12,36 @@ var config = {
 firebase.initializeApp(config);
 
 database = firebase.database();
+
+
+function initMap() {
+
+    var philly = { lat: 39.953, lng: -75.165 };
+
+    var map = new google.maps.Map(
+        document.getElementById('main-map'), { zoom: 13, center: philly });
+    var map2 = new google.maps.Map(
+        document.getElementById('map-input'), { zoom: 13, center: philly });
+
+    var contentString = "<p>Test</p>"
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: philly,
+        map: map2,
+
+    
+
+    });
+    var marker2 = new google.maps.Marker({
+      position: { lat: 39.983, lng: -75.165 },
+      map: map2,
+
+    });
+    marker2.addListener('click', function () {
+        infowindow.open(map2, marker2);
+    });
+}
