e37ad5feb4613f83017137a9b04bb64d53cfe4cc
Cadin McQueen <cadin.mcqueen@gmail.com>
2018-09-29 09:59:12 -0400

    Clean up js and hide api keys using separate file and .gitignore (#24)
    * add inputs on index.html
    
    * Add bootstrap positioning and some temporary quality of life styling
    
    * Added funtioning google maps
    
    * finished user input map
    
    * Add logic that pulls values from form and input map and sends them to Firebase as an object
    
    * google maps locations from firebase data
    
    * further intergrated google maps with firebase
    
    * Add form validation and cleaned up some js variable names
    
    * Add data validation and error messages
    
    * Fix form submission bug
    
    * Add formatting to the user inputs before pushing to Firebase
    
    * reformatted app.js to pet-finder
    
    * Integrate openweathermap API
    
    * Updated form
    
    * Bugfixes
    
    * minor changes to wording and syntax
    
    * minor changes to assets
    
    * made changes
    
    * Info window tweaks
    
    * More tweaks to info window
    
    * Add closing functionality on info windows. Add additional colors to info window.
    
    * Add pet type back into infoWindow
    
    * changed background
    
    * Cleaned up front end display, added google maps location
    
    * added more functionality tothe map input and main map
    
    * bug fix
    
    * updated master
    
    * Add favicon and missing placeholder text
    
    * Update Readme
    
    * Fix headers in readme
    
    * Reorganize js into functions for easier readability
    
    * Hide firebase API key using .gitignore

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,7 @@
-var config = {
-  apiKey: "AIzaSyDDqeSFBecW0z7ho68WE5ZwcTusl6hVits",
-  authDomain: "musician-finder-f0e28.firebaseapp.com",
-  databaseURL: "https://musician-finder-f0e28.firebaseio.com",
-  projectId: "musician-finder-f0e28",
-  storageBucket: "",
-  messagingSenderId: "495490109784"
-};
-
 firebase.initializeApp(config);
 database = firebase.database();
 
+// Declare Maps/forms variables
 var latInput = 0;
 var longInput = 0;
 var mainMap;
@@ -17,15 +9,34 @@ var inputMap;
 var markerPlaced = false;
 var formCompleted = false;
 
-function initMap() {
-  var currentLocation = { lat: 39.953, lng: -75.165 };
+// Declare pet variables
+var name;
+var phone;
+var petName;
+var petType;
+var petAge;
+var petImage;
+var description;
 
+// Declare weather variables
+var maxTemp;
+var minTemp;
+var currentTemp;
+var weatherMessage;
+
+function initMap() {
+  var currentLocation = { lat: 39.953, lng: -75.165 }; // default location: Philadelphia
+
+  // If the user allows current location detection...
   if (navigator.geolocation) {
+    // Get current location and reassign the currentLocation var
     navigator.geolocation.getCurrentPosition(function (position) {
       currentLocation = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
+
+      // Pan both maps to the user's location
       mainMap.panTo(currentLocation);
       inputMap.panTo(currentLocation);
     }, function () {
@@ -36,7 +47,6 @@ function initMap() {
     handleLocationError(false, infoWindow, map.getCenter());
   }
 
-
   mainMap = new google.maps.Map(
     document.getElementById('main-map'), { zoom: 13, center: currentLocation });
   inputMap = new google.maps.Map(
@@ -45,15 +55,19 @@ function initMap() {
   var newMarker;
 
   function placeMarker(location) {
+
     if (newMarker) {
+      // If the marker has already be placed, update the marker's position
       newMarker.setPosition(location);
     } else {
+      // If the marker has not been placed, create a new marker
       newMarker = new google.maps.Marker({
         position: location,
         map: inputMap
       });
     }
 
+    // Store the marker's coordinates
     latInput = newMarker.getPosition().lat();
     longInput = newMarker.getPosition().lng();
   }
@@ -67,8 +81,66 @@ function initMap() {
   });
 }
 
+function generateNewPet() {
+  // Create an object with all the relevant, formatted data
+  var newPet = {
+    name: nameFormatted,
+    phone: phone,
+    petName: petNameFormatted,
+    petType: petTypeFormatted,
+    petAge: petAgeFormatted,
+    petImage: petImage,
+    description: descriptionFormatted,
+    position: {
+      lat: latInput,
+      long: longInput
+    }
+  }
+
+  // Push the pet object to Firebase
+  database.ref().push(newPet);
+}
+
+function generateWeatherMessage() {
+  // Set the weather message depending on the temperature
+  if (maxTemp > 85) {
+    weatherMessage = "<span class=\"red\">Warning:</span> the max temperature in this area for today is <span class=\"red\">" +
+      Math.round(maxTemp) + "&#176;F</span>.";
+  } else if (minTemp < 32) {
+    weatherMessage = "<span class=\"red\">Warning:</span> The low temperature in this area for today is <span class=\"blue\">" +
+      Math.round(minTemp) + "&#176;F</span>.";
+  } else {
+    weatherMessage = "The current temperature in this area is <span class=\"green\">" + Math.round(currentTemp) + "&#176;F</span>.";
+  }
+}
+
 $(document).ready(function () {
 
+  function checkFormCompletion() {
+    // Store form values
+    name = $('#name-input').val().trim();
+    phone = $('#phone-input').val().trim();
+    petName = $('#pet-name-input').val().trim();
+    petType = $('#pet-type-input').val().trim()
+    petAge = $('#pet-age-input').val().trim();
+    petImage = $('#pet-image-input').val().trim();
+    description = $('#description-input').val().trim();
+
+    // If all input fields have been filled out...
+    if (name != "" && phone != "" && petName != "" && petType != "" && petAge != "" && petImage != "" && description != "") {
+      // Format the user input to prepare it for storage in Firebase
+      nameFormatted = name.charAt(0).toUpperCase() + name.substr(1);
+      petNameFormatted = petName.charAt(0).toUpperCase() + petName.substr(1);
+      petTypeFormatted = petType.charAt(0).toUpperCase() + petType.substr(1);
+      petAgeFormatted = petAge.charAt(0).toUpperCase() + petAge.substr(1);
+      descriptionFormatted = description.charAt(0).toUpperCase() + description.substr(1);
+
+      // Mark the form as completed
+      formCompleted = true;
+    }
+  }
+
+  // Submit the form
   $("#submit-btn").on("click", function (event) {
 
     // if no marker was placed, prevent form submission
@@ -76,60 +148,14 @@ $(document).ready(function () {
       event.preventDefault();
     }
 
-    var name;
-    var phone;
-    var petName;
-    var petType;
-    var petAge;
-    var petImage;
-    var description;
-
-    function checkFormCompletion() {
-      name = $('#name-input').val().trim();
-      phone = $('#phone-input').val().trim();
-      petName = $('#pet-name-input').val().trim();
-      petType = $('#pet-type-input').val().trim()
-      petAge = $('#pet-age-input').val().trim();
-      petImage = $('#pet-image-input').val().trim();
-      description = $('#description-input').val().trim();
-
-      // If all input fields have been filled out...
-      if (name != "" && phone != "" && petName != "" && petType != "" && petAge != "" && petImage != "" && description != "") {
-        // Format the user input to prepare it for storage in Firebase
-        nameFormatted = name.charAt(0).toUpperCase() + name.substr(1);
-        petNameFormatted = petName.charAt(0).toUpperCase() + petName.substr(1);
-        petTypeFormatted = petType.charAt(0).toUpperCase() + petType.substr(1);
-        petAgeFormatted = petAge.charAt(0).toUpperCase() + petAge.substr(1);
-        descriptionFormatted = description.charAt(0).toUpperCase() + description.substr(1);
-
-        // Mark the form as completed
-        formCompleted = true;
-      }
-    }
-
+    // Check for form completion
     checkFormCompletion();
 
     // If an input marker has been placed AND the form is completed, add the data to firebase
     if (markerPlaced && formCompleted) {
-
-      // Create an object with all the relevant, formatted data
-      var newPet = {
-        name: nameFormatted,
-        phone: phone,
-        petName: petNameFormatted,
-        petType: petTypeFormatted,
-        petAge: petAgeFormatted,
-        petImage: petImage,
-        description: descriptionFormatted,
-        position: {
-          lat: latInput,
-          long: longInput
-        }
-      }
-
-      // Push the musician object to Firebase
-      database.ref().push(newPet);
+      generateNewPet();
     } else {
+      // Display the error message if no pin was placed
       $('#error').removeClass("d-none");
     }
   });
@@ -149,12 +175,6 @@ $(document).ready(function () {
     var petImage = childData.petImage;
     var petDescription = childData.description;
 
-    // Set weather variables
-    var maxTemp;
-    var minTemp;
-    var currentTemp;
-    var weatherMessage;
-
     // Place a marker based on the object's position
     var marker = new google.maps.Marker({
       position: petPosition,
@@ -173,16 +193,7 @@ $(document).ready(function () {
       minTemp = response.main.temp_min;
       currentTemp = response.main.temp;
 
-      // Set the weather message depending on the temperature
-      if (maxTemp > 85) {
-        weatherMessage = "<span class=\"red\">Warning:</span> the max temperature in this area for today is <span class=\"red\">" +
-          Math.round(maxTemp) + "&#176;F</span>.";
-      } else if (minTemp < 32) {
-        weatherMessage = "<span class=\"red\">Warning:</span> The low temperature in this area for today is <span class=\"blue\">" +
-          Math.round(minTemp) + "&#176;F</span>.";
-      } else {
-        weatherMessage = "The current temperature in this area is <span class=\"green\">" + Math.round(currentTemp) + "&#176;F</span>.";
-      }
+      generateWeatherMessage();
 
       // Generate a DOM node to display the data
       var contentString =
